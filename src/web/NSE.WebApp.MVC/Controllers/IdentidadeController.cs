bb70f3c5ec7aadf2edacaa4831692ea543ca626c
jailton.junior <jailton.junior@iterative.com.br>
2020-08-24 20:52:37 -0300

    Inicio da criação do projeto: WebApp.MVC

---
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,58 @@
+﻿using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            // API - Registro
+            if (false) return View(usuarioRegistro);
+
+            // Realizar o login
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            // API - Registro
+            if (false) return View(usuarioLogin);
+
+            // Realizar o login
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
